f4cd66b466955185988ee7b2501e0865c5fc2ef2
kien <kienle371999@gmail.com>
2020-08-16 21:41:15 +0700

    heroku deploy

---
+++ backend/dist/precache-manifest.1b7bc48f603a475ba3da00a562eeff2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1c82be4f0bfc1d40b4c",
+    "url": "/css/app.1fc54930.css"
+  },
+  {
+    "revision": "e7f9f312e78a402b0307181557c01c0e",
+    "url": "/img/icon.e7f9f312.png"
+  },
+  {
+    "revision": "d7d5a0a20b74d12adcd5a1a0934739a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1c82be4f0bfc1d40b4c",
+    "url": "/js/app.b486ea1b.js"
+  },
+  {
+    "revision": "682170a22715dbf806ad",
+    "url": "/js/chunk-vendors.9e073562.js"
+  },
+  {
+    "revision": "4b14c64efaf846819b9a229b4193c8b7",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
