acc00ae4902a37931d2cedb5e28730ea3191777e
kien <kienle371999@gmail.com>
2020-08-18 10:40:41 +0700

    push

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const  express = require('express')
 const path = require('path')
 const serveStatic = require('serve-static')
+const httpServer = require('http-server')
+const history = require('connect-history-api-fallback')
 
 const app = express()
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 7000
 
-app.use(serveStatic(__dirname + '/dist'))
+app.use(express.static(__dirname + '/frontend/dist'))
+app.use(history({
+    disableDotRule: true,
+    verbose: true
+}))
+app.use(express.static(__dirname + '/frontend/dist'))
+
 app.listen(port, function() {
     console.log(`Server is listened on port ${port}`)
 })
